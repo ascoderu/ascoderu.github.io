1f3262c1db47085e4a083102e0c20a09a69fdf01
Laura Barluzzi <laura@koalacoder.com>
2017-11-15 09:30:03 -0500

    polish style and add first layer of flexbox

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function checkDevice(height, width) {
   var small = [800, 1000];
   var heightIsLower = height < small[0];
@@ -11,7 +9,7 @@ function setUpFullpage(screenSize) {
   $('#fullpage').fullpage({
     anchors: ['0-home-page', '1-about', '2-take-action', '3-media'],
     sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', '#000'],
-    menu:"#myNavbar",
+    menu:"#navbar-menu",
     scrollBar: true,
     responsiveHeight: screenSize[0],
     responsiveWidth: screenSize[1],
@@ -23,19 +21,14 @@ $(document).ready(function() {
   
   var heightScreen = $(window).height(); 
   var widthScreen = $(window).width();
-  console.log(heightScreen + " width = " + widthScreen);
+
   setUpFullpage(checkDevice(heightScreen, widthScreen));      
 
   // So the Mobile Nav Hides When a Link is Clicked
   $(".navbar-nav li a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
   });
-  
-  $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-  });
-  
+
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   });
