553afd22fcca936ee0c85c27d082e067d38bca59
Laura Barluzzi <laura@koalacoder.com>
2018-01-09 10:49:24 -0500

    Creates a completely new website with multiple pages (#3)

---
+++ script.js
@@ -0,0 +1,44 @@
+function setUpFullpage(screenSize) {
+  $('#fullpage').fullpage({
+    anchors: ['home-page', 'history', 'lokole', 'help-us', 'news', 'contact'],
+    menu:"#navbar-menu",
+    paddingTop: '80px',
+    paddingBottom: '60px',
+    responsiveHeight: screenSize.height,
+    responsiveWidth: screenSize.width,
+    afterRender: function() {
+      $("#website-content").removeClass("hidden");
+    }
+  });
+}
+
+function assignModalTitleAndContent(clicked) {
+  var clickedModalId = clicked.data("reference");
+  var contentModalHtml = $("#"+clickedModalId).html();
+
+  BootstrapDialog.show({
+    title: clicked.data("title"),
+    message: $('<div/>').append(contentModalHtml)
+  });
+}
+
+$(document).ready(function() {
+
+  var screenSize = {
+    height: $(window).height(),
+    width: $(window).width()
+  };
+
+  setUpFullpage(screenSize);
+
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $("div[rel='modal']").click(function(event){
+    assignModalTitleAndContent($(this));
+  });
+
+});
