22adff54a0c9a62f18f77160611d1fa1f07770af
Clemens Wolff <clewolff@microsoft.com>
2018-01-13 17:49:46 -0500

    Fix all eslint issues

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-(function() {
+(function($, BootstrapDialog) {
 
-function setUpFullpage() {
-  $('#fullpage').fullpage({
-    anchors: ['home-page', 'history', 'lokole', 'help-us', 'news', 'contact'],
-    menu: '#navbar-menu',
-    paddingTop: '80px',
-    paddingBottom: '60px',
-    responsiveHeight: 700,
-    afterRender: function() {
-      $('#website-content').removeClass('hidden');
-    }
+  function setUpFullpage() {
+    $('#fullpage').fullpage({
+      anchors: ['home-page', 'history', 'lokole', 'help-us', 'news', 'contact'],
+      menu: '#navbar-menu',
+      paddingTop: '80px',
+      paddingBottom: '60px',
+      responsiveHeight: 700,
+      afterRender: function() {
+        $('#website-content').removeClass('hidden');
+      }
+    });
+  }
+
+  function assignModalTitleAndContent($clicked) {
+    var clickedModalId = $clicked.data('reference');
+    var contentModalHtml = $('#' + clickedModalId).html();
+
+    BootstrapDialog.show({
+      title: $clicked.data('title'),
+      message: $('<div/>').append(contentModalHtml)
+    });
+  }
+
+  $(document).ready(function() {
+
+    setUpFullpage();
+
+    $('.navbar-nav li a').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('div[rel="modal"]').click(function() {
+      assignModalTitleAndContent($(this));
+    });
+
   });
-}
 
-function assignModalTitleAndContent($clicked) {
-  var clickedModalId = $clicked.data('reference');
-  var contentModalHtml = $('#' + clickedModalId).html();
-
-  BootstrapDialog.show({
-    title: $clicked.data('title'),
-    message: $('<div/>').append(contentModalHtml)
-  });
-}
-
-$(document).ready(function() {
-
-  setUpFullpage();
-
-  $('.navbar-nav li a').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  $('[data-toggle="tooltip"]').tooltip();
-
-  $('div[rel="modal"]').click(function() {
-    assignModalTitleAndContent($(this));
-  });
-
-});
-
-})();
+})(window.jQuery, window.BootstrapDialog);
